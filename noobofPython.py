0603c7663d546163d0b8a16a8cd165fcf3014381
wanghuinengstil <wanghuineng123@gmail.com>
2020-04-05 00:55:43 +0200

    Update noobofPython.py

--- noobofPython.py
+++ noobofPython.py
@@ -7,5 +7,49 @@ class User1:
 
 	def __str__(self):
 		return "the password is %s"%self._password
-u2=User1("123")
-print(u2)
+
+
+
+class Animal:
+	def __init__(self):
+		self.name="xiaobai"
+	def eat(self):
+		print("eat something")
+	def sleep(self):
+		print("sleep")
+
+class Dog(Animal):
+	def shout(self):
+		print("dog shout")
+class Cat(Animal):
+	def catchRat(self):	
+		print("catch rat")
+
+
+
+class Animal:
+	def __init__(self):
+		self.name="xiaobai"
+	def eat(self):
+		print("eat something")
+	def sleep(self):
+		print("sleep")
+
+class Dog(Animal):
+	def __init__(self,name):
+		self.name=name
+	def shout(self):
+		print("dog shout")
+class Cat(Animal):
+	def __init__(self):
+		print("ready initiated")
+	def catchRat(self):	
+		print("catch rat")
+
+#print(cat.name)
+class BigDog(Dog):
+ 	def fight(self):
+ 		print("Bigdog to fight")
+za=Dog("NiCai")
+za.eat()
+ 
