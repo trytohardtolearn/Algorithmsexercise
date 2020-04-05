d5619f4e204675785ad32b58a757f3aa471f9350
wanghuinengstil <wanghuineng123@gmail.com>
2020-04-05 23:39:27 +0200

    Update noobofPython.py

--- noobofPython.py
+++ noobofPython.py
@@ -29,7 +29,7 @@ class Cat(Animal):
 
 class Animal:
 	def __init__(self):
-		self.name="xiaobai"
+		self.color="xiaobai"
 	def eat(self):
 		print("eat something")
 	def sleep(self):
@@ -37,6 +37,7 @@ class Animal:
 
 class Dog(Animal):
 	def __init__(self,name):
+		super().__init__()#active to use fathers Attribute
 		self.name=name
 	def shout(self):
 		print("dog shout")
@@ -51,5 +52,56 @@ class BigDog(Dog):
  	def fight(self):
  		print("Bigdog to fight")
 za=Dog("NiCai")
-za.eat()
+print(za.name)
+print(za.color)
+
+
+# Duck Sort of Python
+class F1:
+	def show(self):
+		print(F1.show)
+class S1(F1):
+	def show(self):
+		print(S1.show)
+class S2(S1):
+	def show(self):
+		print(S2.show)
+def Func(obj):
+	print (obj.show())
+s1_obj=S1()
+Func(s1_obj)
+s2_obj=S2()
+Func(s2_obj)	
  
+
+class User1(object):
+ 	name="QinAi" #public Attribute
+ 	__password="123456"
+ 	def __init__(self,sex,username):
+ 		self.sex=sex
+ 		self.username=username
+
+u=User1("man","wanghuineng")
+print(u.name)
+print(u.username)
+
+class A(object):
+	name="wanghuineng"
+	def test1(self):
+		print("++"*30)
+	@classmethod
+	def test2(cls):
+		cls.name="Aiqing"
+
+		print("==="*30)
+	@staticmethod 
+	def test3():
+		bane="buzhidao"
+		print(bane)
+		
+a=A()
+a.test2()  
+a.test1()
+print(A.name)
+a.test3()
+
