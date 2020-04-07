ca10e3212748c7b3f1c9d37946fa84cff9292b64
wanghuinengstil <wanghuineng123@gmail.com>
2020-04-08 01:30:49 +0200

    Update noobofPython.py

--- noobofPython.py
+++ noobofPython.py
@@ -105,3 +105,57 @@ a.test1()
 print(A.name)
 a.test3()
 
+
+class Wang(object):
+	def __init__(self,username,password):
+		self.username=username
+		self.password=password
+		print("对象已经构造好了，由解释器自动调用的方法")
+
+	def __new__(cls,username,password):
+		print("类的对象开始创建")
+
+u=Wang("buzhidao","xiaobai")
+print(u)#这里是因为它没有返回当前类的对象，NEW方法里面没有返回，那么执行完第9行就自动终止,对象并没有被真正创建
+
+
+class Wang1(object):
+	def __init__(self,username,password):
+		self.username=username
+		self.password=password
+		print("对象已经构造好了，由解释器自动调用的方法")
+
+	def __new__(cls,username,password):
+
+		print("类的对象开始创建")
+		return object.__new__(cls)
+	def __str__(self):
+		return "用户名为%s，密码为%s"%(self.username,self.password)
+u1=Wang1("buzhidao","wotaibeng")
+print(u1)
+
+class Wang2(object):
+	def __init__(self,name):
+		self.name=name
+w1=Wang2("wanghuineng")
+w2=Wang2("wanghuineng")
+print(w1==w2)
+print(id(w1),id(w2))
+
+
+class Wang3(object):
+	__instance=None
+	def __init__(self,name):
+		self.name=name
+	@classmethod
+	def get_instance(cls,name):
+		if not cls.__instance:
+			cls.__instance=Wang3(name)
+		return cls.__instance
+w3=Wang3.get_instance("wanghuineng")
+w4=Wang3.get_instance("buzhidao")
+print(w3==w4)
+print(id(w3),id(w4))
+
+
+
